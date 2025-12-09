30341f10c6236f727c2fc5de74a019449a3bec0a
DPoroshin <dm.poroshin@gmail.com>
2025-12-09 13:34:23 +0300

    task-1

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,10 @@
 const yaml = require("yaml");
 const fs = require("node:fs");
+
+const extensions = [".yml", ".yaml"];
+
+extensions.forEach(extension => {
+    require.extensions[extension] = (module, filename) => {
+        module.exports = yaml.parse(fs.readFileSync(filename, "utf8"));
+    }
+});
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,38 @@
 import fs from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import yaml from "yaml";
+
+
+export async function resolve(specifier, context, nextResolve) {
+    if (specifier.endsWith('.yml') || specifier.endsWith('.yaml')) {
+        const resolved = await nextResolve(specifier, context);
+
+        return {
+            url: resolved.url,
+            format: 'module',
+            shortCircuit: true,
+        };
+    }
+
+    return nextResolve(specifier, context);
+}
+
+export async function load(url, context, nextLoad) {
+    if (url.endsWith('.yml') || url.endsWith('.yaml')) {
+        const filepath = fileURLToPath(url);
+
+        const content = await fs.readFile(filepath, 'utf8');
+
+        const data = yaml.parse(content.toString());
+
+        const code = `export default ${JSON.stringify(data)}`;
+
+        return {
+            format: 'module',
+            source: code,
+            shortCircuit: true,
+        };
+    }
+
+    return nextLoad(url, context);
+}
