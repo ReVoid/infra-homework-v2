f46d360fcd243cf445a6d0a844a1cdf350f420f3
DPoroshin <dm.poroshin@gmail.com>
2026-03-05 16:30:01 +0300

    task-2 complete

--- .yarn/plugins/plugin-duplicates-list.cjs
+++ .yarn/plugins/plugin-duplicates-list.cjs
@@ -1,7 +1,57 @@
 module.exports = {
-  name: `plugin-duplicates-list`,
+  name: 'plugin-duplicates-list',
   factory: (require) => {
-    const { BaseCommand } = require(`@yarnpkg/cli`);
-    const { Configuration, Project, structUtils } = require("@yarnpkg/core");
+    const { BaseCommand } = require('@yarnpkg/cli');
+    const { Configuration, Project, structUtils } = require('@yarnpkg/core');
+
+    class DuplicatesListCommand extends BaseCommand {
+      static paths = [
+        ['duplicates'],
+      ];
+
+      async execute() {
+        // cwd - current working directory 😉
+        const cwd = await Configuration.findProjectCwd(this.context.cwd);
+        // or just this.context.cwd
+        // const cwd = this.context.cwd;
+
+        const configuration = await Configuration.find(cwd, this.context.plugins);
+
+        const { project} = await Project.find(configuration, cwd);
+
+        await project.restoreInstallState();
+
+        const packages = project.storedPackages
+            .values()
+            .reduce((acc, pkg) => {
+              if (structUtils.isVirtualLocator(pkg)) {
+                return acc;
+              }
+
+              const name = structUtils.stringifyIdent(pkg);
+              const version = pkg.version;
+
+              if (!acc[name]) {
+                acc[name] = [];
+              }
+
+              if (!acc[name].includes(version)) {
+                acc[name].push(version);
+              }
+
+              return acc;
+            }, {});
+
+        const duplicates = Object.entries(packages).filter(([, versions]) => versions.length > 1);
+
+        duplicates.forEach(([name, versions], index) => {
+          this.context.stdout.write(`${index++}. ${name} [${versions.join(', ')}]\n`);
+        });
+      }
+    }
+
+    return {
+      commands: [DuplicatesListCommand],
+    }
   },
 };
